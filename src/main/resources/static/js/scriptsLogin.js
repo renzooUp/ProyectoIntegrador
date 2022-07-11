7abb543665224e6e9e3b40c8f35f7e6f622318c0
renzooUp <renzoupiachihua3@outlook.com>
2022-07-11 15:52:38 -0500

    Modificando una imagen de taller y agregando la pagina de logeo

---
+++ src/main/resources/static/js/scriptsLogin.js
@@ -0,0 +1,60 @@
+function VerificarUsuario(){
+    var txtUsuario = document.getElementById("txtUsuario").value;
+    var txtPassword = document.getElementById("txtPassword").value;
+    
+    if (txtUsuario == "admin" && txtPassword == "admin") {
+        console.log(txtUsuario + txtPassword);
+        window.location.href = "/admin";
+    } else {
+        console.log("ta mal xd ");
+        console.log(txtUsuario + txtPassword);
+    }
+};
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
